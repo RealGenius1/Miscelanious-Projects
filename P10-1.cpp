28b83c049a80e72fed38fd3e7ed13561023c6009
RealGenius1 <sptho24@gmail.com>
2024-11-04 13:44:31 -0500

    MP-3

---
+++ P10-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int count_words(string str);
+
+int main(){
+
+}
+
+int count_words(string str){
+    int count = 0;
+    for(int i = 0; i != NULL; i++){
+
+    }
+}
