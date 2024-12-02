0f08ba9e32ed2d3805982a54568ec7ba913196e9
RealGenius1 <sptho24@gmail.com>
2024-12-02 18:46:27 -0500

    formatting

--- Prog 1/P13-2.cpp
+++ Prog 1/P13-2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 bool isLeapYear(int year);
 /**
    isLeapYear - determines whether a given year is a leap year
-                under the Gregorian calendar. 
+                under the Gregorian calendar.
    @param year the year; expected to be >= 1582
    @return true if year is a leap year; false otherwise
 */
@@ -30,53 +30,75 @@ int dayOfWeek(int month, int day, int year);
     @return the weekday (0 = Saturday ... 6 = Friday)
 */
 
-int main() {
+int main()
+{
     int year = 0;
     int month = 0;
     string monthName = "";
     string dayName = "";
 
-//                          Sa Fr Th We Tu Mo Su
-// returned by DoW funct.:  00 01 02 03 04 05 06 
-// character positions:     012345678901234567890123456789012345678
-    string calendar[6] = { "                   1  2  3  4  5  6  7 ",
-                           " 2  3  4  5  6  7  8  9 10 11 12 13 14 ",
-                           " 9 10 11 12 13 14 15 16 17 18 19 20 21 ",
-                           "16 17 18 19 20 21 22 23 24 25 26 27 28 ",
-                           "23 24 25 26 27 28 29 30 31             ",
-                           "30 31                                  " };
+    //                          Sa Fr Th We Tu Mo Su
+    // returned by DoW funct.:  00 01 02 03 04 05 06
+    // character positions:     012345678901234567890123456789012345678
+    string calendar[6] = {"                   1  2  3  4  5  6  7 ",
+                          " 2  3  4  5  6  7  8  9 10 11 12 13 14 ",
+                          " 9 10 11 12 13 14 15 16 17 18 19 20 21 ",
+                          "16 17 18 19 20 21 22 23 24 25 26 27 28 ",
+                          "23 24 25 26 27 28 29 30 31             ",
+                          "30 31                                  "};
 
-    while(true) {
+    while (true)
+    {
         cout << "Enter a month and a year or Q to quit: ";
         cin >> monthName;
-        if(monthName == "Q") break;
+        if (monthName == "Q")
+            break;
         cin >> year;
-        if(cin.fail()) break;
+        if (cin.fail())
+            break;
 
-        if(monthName == "January")   month = 1;
-        if(monthName == "February")  month = 2;
-        if(monthName == "March")     month = 3;
-        if(monthName == "April")     month = 4;
-        if(monthName == "May")       month = 5;
-        if(monthName == "June")      month = 6;
-        if(monthName == "July")      month = 7;
-        if(monthName == "August")    month = 8;
-        if(monthName == "September") month = 9;
-        if(monthName == "October")   month = 10;
-        if(monthName == "November")  month = 11;
-        if(monthName == "December")  month = 12;
-        
+        if (monthName == "January")
+            month = 1;
+        if (monthName == "February")
+            month = 2;
+        if (monthName == "March")
+            month = 3;
+        if (monthName == "April")
+            month = 4;
+        if (monthName == "May")
+            month = 5;
+        if (monthName == "June")
+            month = 6;
+        if (monthName == "July")
+            month = 7;
+        if (monthName == "August")
+            month = 8;
+        if (monthName == "September")
+            month = 9;
+        if (monthName == "October")
+            month = 10;
+        if (monthName == "November")
+            month = 11;
+        if (monthName == "December")
+            month = 12;
+
         int firstDay = dayOfWeek(month, 1, year);
         int days = daysInMonth(month, year);
         int currentDay = 1;
 
-        cout << endl << monthName << " " << year << endl;
+        cout << endl
+             << monthName << " " << year << endl;
         cout << "Sa Fr Th We Tu Mo Su" << endl;
-        for(int i = 0; i < 6; i++) {
-            for(int j = 0; j < 7; j++) {
-                if(i == 0 && j < firstDay - 1) { // If it's the first week and we're before the first day of the month
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                if (i == 0 && j < firstDay - 1)
+                {                  // If it's the first week and we're before the first day of the month
                     cout << "   "; // Print empty space
-                } else if(currentDay <= days) { // If we're within the valid range of days for the month
+                }
+                else if (currentDay <= days)
+                { // If we're within the valid range of days for the month
                     cout << setw(2) << currentDay << " ";
                     currentDay++;
                 }
@@ -89,7 +111,8 @@ int main() {
         cout << "Output file: ";
         cin >> datafile;
         ofstream file(datafile);
-        if(!file) {
+        if (!file)
+        {
             cout << "Unable to open or create file.";
             return 1;
         }
@@ -98,11 +121,16 @@ int main() {
         file << monthName << " " << year << endl;
         file << "Sa Fr Th We Tu Mo Su" << endl;
         currentDay = 1;
-        for(int i = 0; i < 6; i++) {
-            for(int j = 0; j < 7; j++) {
-                if(i == 0 && j < firstDay - 1) { // If it's the first week and we're before the first day of the month
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                if (i == 0 && j < firstDay - 1)
+                {                  // If it's the first week and we're before the first day of the month
                     file << "   "; // Print empty space
-                } else if(currentDay <= days) { // If we're within the valid range of days for the month
+                }
+                else if (currentDay <= days)
+                { // If we're within the valid range of days for the month
                     file << setw(2) << currentDay << " ";
                     currentDay++;
                 }
@@ -111,31 +139,52 @@ int main() {
         }
     }
 }
-bool isLeapYear(int year) {
-    if((year % 4 == 0) && !(year % 100 == 0)) return true;
-    else if((year % 100 == 0) && (year % 400 == 0)) return true;
-    else return false;
+bool isLeapYear(int year)
+{
+    if ((year % 4 == 0) && !(year % 100 == 0))
+        return true;
+    else if ((year % 100 == 0) && (year % 400 == 0))
+        return true;
+    else
+        return false;
 }
-int daysInMonth(int month, int year) {
-    if(month == 2) { // Check for February
-        if(isLeapYear(year)) return 29;
-        else return 28;
+int daysInMonth(int month, int year)
+{
+    if (month == 2)
+    { // Check for February
+        if (isLeapYear(year))
+            return 29;
+        else
+            return 28;
     }
-    else { // All other months
-        switch(month) {
-            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                return 31;
-            case 4: case 6: case 9: case 11:
-                return 30;
+    else
+    { // All other months
+        switch (month)
+        {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
         }
     }
     return 31;
 }
 
-int dayOfWeek(int month, int day, int year) {
+int dayOfWeek(int month, int day, int year)
+{
     int modMonth = month;
     int modYear = year;
-    if(month == 1 || month == 2) {
+    if (month == 1 || month == 2)
+    {
         modMonth += 12;
         modYear--;
     }
