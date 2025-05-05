5e8a894d39b9973d9b73e2b9836854836dc3a34d
RealGenius1 <sptho24@gmail.com>
2025-05-05 09:31:04 -0400

    bogosort without algorithm inclusion

--- DSA 1/bogosort.cpp
+++ DSA 1/bogosort.cpp
@@ -1,34 +1,39 @@
-#include <iostream>
-#include <algorithm>
 #include <random>
+#include <iostream>
 
 using namespace std;
 
+void shuffle(int A[], int n){
+    for(int i = 0; i < n; i++){
+        int j = rand() % (n - i) + i;
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
 
 bool isSorted(int A[], int n){
-    for(int i = 1; i < n; i++){
-        if(A[i-1] > A[i]){
+    for(int i = 0; i < n - 1; i++){
+        if(A[i] > A[i+1]){
             return false;
         }
     }
     return true;
 }
 
-void bogosort(int A[], int n, int& count){
-    count++;
-    random_shuffle(A, A+n);
-    if(!isSorted(A,n)){
-        bogosort(A,n,count);
+void bogosort(int A[], int n){
+    while(!isSorted(A,n)){
+        shuffle(A,n);
     }
 }
 
-int main(){
+int main() {
     srand(time(0));
-    int count = 0;
-    int A[] = {1,2,3,4,5};
-    bogosort(A,5, count);
-    for(int i = 0; i < 5; i++){
+    int A[] = {1,4,5,9,7};
+    int n = 5;
+    bogosort(A,5);
+    for(int i = 0; i < n - 1; i++){
         cout << A[i] << ", ";
     }
-    cout << endl << "Count: " << count;
+    cout << A[n-1];
 }
